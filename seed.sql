0af87aa5af93e32a500f892b82bfc01164d8a1b4
John Hinojosa <hinojohn@ymail.com>
2021-04-18 17:47:42 -0500

    Added more seed data and slight alterations to DB schema

--- seed.sql
+++ seed.sql
@@ -1,10 +1,19 @@
 USE staffDB;
 
 INSERT INTO department (name)
-VALUES ('marketing'), ('sales'), ('engineering');
+VALUES ('Sales'), ('Engineering'), ('Legal'), ('Finance');
 
-INSERT INTO role (title, salary)
-VALUES ('manager', 80000.00), ('sales lead', 75000.00);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Salesperson', 80000.00, 1), ('Sales Lead', 100000.00, 1),
+('Engineer', 125000, 2), ('Lead Engineer', 150000, 2),
+('Lawyer', 190000, 3), ('Legal Team Lead', 250000, 3),
+('Accountant', 125000, 4);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ('Bill', 'Preston'), ('Ted', 'Logan');
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Bill', 'Preston', 3), ('Ted', 'Logan', 4), 
+('Cheryl', 'Mason', 7), ('Dash', 'Rendar', 1),
+('Rinoa', 'Heartilly', 6);
+
+UPDATE employee
+SET manager_id = 2
+WHERE id = 1 AND 4;
